ff73a77c5568442165a78ecaa6b6de84445cfb94
Jaime Minang <elejainic@gmail.com>
2014-05-03 09:06:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quedada::Application.config.secret_token = 'ae7e876cd5e9f627183afef8669387e174e0a499ff9796f2391eb553c78f71071edc05258c284b947bc87057b0ac9bb38cef20b56aa264f7368abc026d4f09dc'
